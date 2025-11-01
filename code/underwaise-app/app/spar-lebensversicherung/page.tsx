2db1874662ef7c7c82df507540dd7074bbc7d9df
regjep <arifi@regjep.com>
2025-11-01 15:37:07 +0100

    feat: Implement multi-step form with dynamic fields and validation; add new data schema and update styles

--- code/underwaise-app/app/spar-lebensversicherung/page.tsx
+++ code/underwaise-app/app/spar-lebensversicherung/page.tsx
@@ -1,5 +1,11 @@
 import { LifeStarForm } from "@/components/form/life-start-form";
+import { MultiStepForm } from "@/components/form/multi-step-form";
+import schema from "@/components/form/data.json";
+import { Section } from "@/types";
 
 export default function Form() {
-  return <LifeStarForm />;
+  const sections = schema.sections as Section[];
+
+  // return <LifeStarForm />;
+  return <MultiStepForm sections={sections} />;
 }
