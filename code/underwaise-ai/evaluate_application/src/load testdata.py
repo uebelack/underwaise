d5f3e843a0daf17ec6338247c77b5725b260eef0
Sebastian Hennig <hes@nag.ch>
2025-11-01 19:51:16 +0100

    refactor test data load to db

--- code/underwaise-ai/evaluate_application/src/load testdata.py
+++ code/underwaise-ai/evaluate_application/src/load testdata.py
@@ -5,7 +5,7 @@ conn = pyodbc.connect(os.getenv("SQL_CONN_STRING"))
 
 
 
-with open('../../../../assets/application_and_feature_inserts_2.sql', 'r') as inserts:
+with open('../../../../assets/application_and_feature_inserts.sql', 'r') as inserts:
     sql_script = inserts.read()  # read the entire file as a single string
     statements = sql_script.split(';')  # split by semicolon
 
